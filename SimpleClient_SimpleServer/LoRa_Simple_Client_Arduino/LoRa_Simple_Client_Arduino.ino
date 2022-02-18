2bc756f15017b91d0d0ede763166026b5d5d56c3
trevorever1 <98439239+trevorever1@users.noreply.github.com>
2022-02-18 15:37:02 -0600

    Create LoRa_Simple_Server_Yun
    The Dragino gateway will listen for a message from the Arduino Uno + LoRa shield on a frequency of 915 MHz then respond with a message.

--- SimpleClient_SimpleServer/LoRa_Simple_Client_Arduino/LoRa_Simple_Client_Arduino.ino
+++ SimpleClient_SimpleServer/LoRa_Simple_Client_Arduino/LoRa_Simple_Client_Arduino.ino
@@ -1,6 +1,35 @@
 /*
   LoRa Simple Client for Arduino :
   Support Devices: LoRa Shield + Arduino 
+  
+  Example sketch showing how to create a simple messageing client, 
+  with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+  reliability, so you should only use RH_RF95 if you do not need the higher
+  level messaging abilities.
+
+  It is designed to work with the other example LoRa Simple Server
+  User need to use the modified RadioHead library from:
+  https://github.com/dragino/RadioHead
+
+  modified 16 11 2016
+  by Edwin Chen <support@dragino.com>
+  Dragino Technology Co., Limited
+*/
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+float frequency = 915.0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  while (!Serial) ; // Wait for serial port to be available
+  Serial.println("Start LoRa Client");
+  if (!rf95.init())
+    Serial.println("init failed");
   // Setup ISM frequency
   rf95.setFrequency(frequency);
   // Setup Power,dBm
