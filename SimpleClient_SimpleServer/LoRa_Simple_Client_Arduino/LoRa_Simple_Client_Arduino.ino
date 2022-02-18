a369a0ad92791b6ef6d87392880c0d84842a63e6
trevorever1 <98439239+trevorever1@users.noreply.github.com>
2022-02-18 15:34:39 -0600

    Create LoRa_Simple_Client_Arduino.ino
    The Arduino Uno + LoRa Shield send a message to the Dragino gateway using LoRa frequency of 915 MHz

---
+++ SimpleClient_SimpleServer/LoRa_Simple_Client_Arduino/LoRa_Simple_Client_Arduino.ino
@@ -0,0 +1,48 @@
+/*
+  LoRa Simple Client for Arduino :
+  Support Devices: LoRa Shield + Arduino 
+  // Setup ISM frequency
+  rf95.setFrequency(frequency);
+  // Setup Power,dBm
+  rf95.setTxPower(13);
+
+  // Setup Spreading Factor (6 ~ 12)
+  rf95.setSpreadingFactor(7);
+  
+  // Setup BandWidth, option: 7800,10400,15600,20800,31200,41700,62500,125000,250000,500000
+  //Lower BandWidth for longer distance.
+  rf95.setSignalBandwidth(125000);
+  
+  // Setup Coding Rate:5(4/5),6(4/6),7(4/7),8(4/8) 
+  rf95.setCodingRate4(5);
+}
+
+void loop()
+{
+  Serial.println("Sending to LoRa Server");
+  // Send a message to LoRa Server
+  uint8_t data[] = "Hello there!";
+  rf95.send(data, sizeof(data));
+  
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (rf95.waitAvailableTimeout(3000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {      Serial.println("recv failed");    }
+  }
+  else
+  {    Serial.println("No reply, is LoRa server running?");  }
+  delay(5000);
+}
